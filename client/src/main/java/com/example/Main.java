050209eef7651eec07a40fbcb5c4a8da7a44d9d2
lai2-24 <lai2-24@itismeucci.com>
2024-10-03 13:39:50 +0200

    terzo commit

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("client partito");
+        Socket s = new Socket("localhost",3000);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        
+        out.writeBytes("forza viola" + '\n');
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta è: "+ stringaRicevuta);
+
+        s.close();
+
+        System.out.println("client finito");
+
+    }   
 }
